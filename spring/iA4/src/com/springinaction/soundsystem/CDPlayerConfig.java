c5c3cd2b5b77925583c2adb8d5eccb899f02cb0f
sagittaria <shuyang_x@hotmail.com>
2017-07-26 02:03:30 +0800

    spring - 理解下自动装配

--- spring/iA4/src/com/springinaction/soundsystem/CDPlayerConfig.java
+++ spring/iA4/src/com/springinaction/soundsystem/CDPlayerConfig.java
@@ -1,18 +1,19 @@
 package com.springinaction.soundsystem;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @Configuration
+@ComponentScan
 public class CDPlayerConfig {
-    @Bean
-    public CompactDisc sgtPeppers() {
-        return new SgtPeppers();
-    }
-    
-    @Bean
-    public CDPlayer cdPlayer(CompactDisc cd){
-        return new CDPlayer(sgtPeppers());
-    }
+//    @Bean
+//    public CompactDisc compactDisc() {
+//        return new SgtPeppers();
+//    }
+//    
+//    @Bean
+//    public CDPlayer cdPlayer(CompactDisc cd){
+//        return new CDPlayer(cd);
+//    }
 }
