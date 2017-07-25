6fa93495288340ad54be271343b3d6317a92bf72
sagittaria <shuyang_x@hotmail.com>
2017-07-25 14:03:58 +0800

    spring - 通过JavaConfig显示装配

--- spring/iA4/src/com/springinaction/soundsystem/CDPlayerConfig.java
+++ spring/iA4/src/com/springinaction/soundsystem/CDPlayerConfig.java
@@ -1,10 +1,18 @@
 package com.springinaction.soundsystem;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @Configuration
-@ComponentScan
 public class CDPlayerConfig {
-
+    @Bean
+    public CompactDisc sgtPeppers() {
+        return new SgtPeppers();
+    }
+    
+    @Bean
+    public CDPlayer cdPlayer(CompactDisc cd){
+        return new CDPlayer(sgtPeppers());
+    }
 }
