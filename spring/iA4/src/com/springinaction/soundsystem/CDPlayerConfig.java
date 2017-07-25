c47794a446c3884229e7d98f5ea6755c6efaa592
sagittaria <shuyang_x@hotmail.com>
2017-07-25 11:25:25 +0800

    spring - 通过注解启用组件扫描

---
+++ spring/iA4/src/com/springinaction/soundsystem/CDPlayerConfig.java
@@ -0,0 +1,10 @@
+package com.springinaction.soundsystem;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
+
+@Configuration
+@ComponentScan
+public class CDPlayerConfig {
+
+}
---
+++ spring/iA4/src/com/springinaction/soundsystem/CompactDisc.java
@@ -0,0 +1,5 @@
+package com.springinaction.soundsystem;
+
+public interface CompactDisc {
+    void play();
+}
