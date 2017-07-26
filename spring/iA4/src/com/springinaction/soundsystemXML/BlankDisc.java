13c46fba5e4c70045890bfc549bd368adc56cb0d
sagittaria <shuyang_x@hotmail.com>
2017-07-26 15:19:54 +0800

    spring - 通过XML装配bean(二)设置属性：注入引用、字面量，分别用property标签、p-命名空间

--- spring/iA4/src/com/springinaction/soundsystemXML/BlankDisc.java
+++ spring/iA4/src/com/springinaction/soundsystemXML/BlankDisc.java
@@ -7,9 +7,15 @@ public class BlankDisc implements CompactDisc {
     private String artist;
     private List<String> tracks;
 
-    public BlankDisc(String title, String artist, List<String> tracks) {
+    public void setTitle(String title) {
         this.title = title;
+    }
+
+    public void setArtist(String artist) {
         this.artist = artist;
+    }
+
+    public void setTracks(List<String> tracks) {
         this.tracks = tracks;
     }
 
