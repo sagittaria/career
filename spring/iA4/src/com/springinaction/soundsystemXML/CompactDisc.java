8b0172392714f329d305b68629c8b0342483faa2
sagittaria <shuyang_x@hotmail.com>
2017-07-26 11:03:35 +0800

    spring - 通过XML装配bean(一)

---
+++ spring/iA4/src/com/springinaction/soundsystemXML/CompactDisc.java
@@ -0,0 +1,5 @@
+package com.springinaction.soundsystemXML;
+
+public interface CompactDisc {
+    void play();
+}
---
+++ spring/iA4/src/com/springinaction/soundsystemXML/MediaPlayer.java
@@ -0,0 +1,5 @@
+package com.springinaction.soundsystemXML;
+
+public interface MediaPlayer {
+    public void play();
+}
