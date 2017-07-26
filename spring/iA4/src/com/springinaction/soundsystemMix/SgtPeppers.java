85076ab821151c3c05aa491f0f98c1b6740617b3
sagittaria <shuyang_x@hotmail.com>
2017-07-26 16:38:18 +0800

    spring - 混合配置-2.在JavaConfig中ImportResource装配在xml中定义的bean

--- spring/iA4/src/com/springinaction/soundsystemMix/SgtPeppers.java
+++ spring/iA4/src/com/springinaction/soundsystemMix/SgtPeppers.java
@@ -1,10 +1,10 @@
-package com.springinaction.soundsystemMix;
-
-public class SgtPeppers implements CompactDisc {
-    private String title = "Sgt. Pepper's Lonely Hearts Club Band";
-    private String artist = "The Beatles";
-
-    public void play() {
-        System.out.println("Playing " + title + " by " + artist);
-    }
-}
+//package com.springinaction.soundsystemMix;
+//
+//public class SgtPeppers implements CompactDisc {
+//    private String title = "Sgt. Pepper's Lonely Hearts Club Band";
+//    private String artist = "The Beatles";
+//
+//    public void play() {
+//        System.out.println("Playing " + title + " by " + artist);
+//    }
+//}
--- spring/iA4/src/com/springinaction/soundsystemMix/SoundSystemConfig.java
+++ spring/iA4/src/com/springinaction/soundsystemMix/SoundSystemConfig.java
@@ -2,9 +2,11 @@ package com.springinaction.soundsystemMix;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@Import({CDPlayerConfig.class,CompactDiscConfig.class})
+@Import(CDPlayerConfig.class)
+@ImportResource("classpath:com/springinaction/soundsystemMix/cd-config.xml")
 public class SoundSystemConfig {
 
 }
