605b97b1b5b54ea7f268265a34795831f21119e4
sagittaria <shuyang_x@hotmail.com>
2017-07-27 11:21:24 +0800

    springMVC - 成功启动应用(tomcat7)

---
+++ spring/iA4Part2/src/spittr/web/HomeControllerTest.java
@@ -0,0 +1,13 @@
+package spittr.web;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import spittr.web.HomeController;
+
+public class HomeControllerTest {
+    @Test
+    public void testHomePage() throws Exception {
+        HomeController controller = new HomeController();
+        assertEquals("home", controller.home());
+    }
+}
